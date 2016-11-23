d26cb39010424426e0baf1de2a782337c5122e27
Valorad <wcxaaa@gmail.com>
2016-11-23 14:12:41 +0800

    + 忽略npm组件

--- ng-src/src/app/app.module.ts
+++ ng-src/src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'; 
 
+import { tBodyRoutingModule } from './tBody-routing.module'; 
 import { AppComponent } from './app.component';
+import { ISCComponent } from './i-sc/i-sc.component';
+import { IndexComponent } from './index/index.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ISCComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    tBodyRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
