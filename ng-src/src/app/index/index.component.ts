14b135edf29002222c40c4607b094472d20af996
Valorad <wcxaaa@gmail.com>
2016-12-04 17:06:41 +0800

    * 修复 issue#1
    修复 issue#1

--- ng-src/src/app/index/index.component.ts
+++ ng-src/src/app/index/index.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var BMap: any;
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
@@ -10,6 +12,9 @@ export class IndexComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var map = new BMap.Map("divTBMap");          // 创建地图实例  
+    var point = new BMap.Point(104.0650, 30.6578);  // 创建点坐标  
+    map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别  
   }
 
 }
