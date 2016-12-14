b48c31d218d75f75982c48385e332badf705b994
Crucsoul Dragonborn <longyiszh@gmail.com>
2016-12-14 13:27:59 +0800

    + 加入谷哥哥地图支持

--- ng-src/src/app/app.module.ts
+++ ng-src/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router'; 
+
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 import { tBodyRoutingModule } from './tBody-routing.module'; 
 
@@ -11,6 +14,8 @@ import { AppComponent } from './app.component';
 
 import { IndexComponent } from './index/index.component';
 import { GymDetailComponent } from './gym/gym-detail/gym-detail.component';
+import { GMapComponent } from './gmap/gmap.component';
+import { BMapComponent } from './bmap/bmap.component';
 
 
 
@@ -21,14 +26,20 @@ import { GymDetailComponent } from './gym/gym-detail/gym-detail.component';
     AppComponent,
 GymDetailComponent,
     IndexComponent,
+    GMapComponent,
+    BMapComponent,
 
    
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule,
     HttpModule,
     RouterModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBikYuz3yf-C5oP6XVyqLq3eYtvuUp_nkk'
+    }),
     tBodyRoutingModule,
     GymModule
   ],
