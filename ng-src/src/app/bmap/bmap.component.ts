a4d59e8c6bc82e98dd227e67c9d47ca78875722a
Valorad <wcxaaa@gmail.com>
2016-12-20 16:33:02 +0800

    + 尝试给健身房加入弹出信息框 但是有问题

--- ng-src/src/app/bmap/bmap.component.ts
+++ ng-src/src/app/bmap/bmap.component.ts
@@ -2,10 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 import * as $ from "jquery";
 declare var BMap: any;
+declare var BMapLib: any;
+declare var BMAPLIB_TAB_SEARCH:any;
+declare var BMAPLIB_TAB_TO_HERE:any;
+declare var BMAPLIB_TAB_FROM_HERE:any;
 
-interface IGym {
+interface IGymLocation {
   lati: number;
   longi: number;
+}
+
+interface IGymInfo {
+  description: string;
 }
 
 @Component({
@@ -21,10 +29,16 @@ export class BMapComponent implements OnInit {
 
   //前台模拟数据
 
-  public gyms: {[id: number]: IGym;} = {
+  public gymsLocation: {[id: number]: IGymLocation;} = {
     0: {lati: 30.676, longi: 104.004},
     1: {lati: 30.613, longi: 104.068},
     2: {lati: 30.653, longi: 104.056}
+  }
+
+  public gymsInfo: {[id: number]: IGymInfo;} = {
+    0: {description: "豪华健身房"},
+    1: {description: "舒适健身房"},
+    2: {description: "亲民价健身房"}
   }
 
   ngOnInit() { 
@@ -33,7 +47,7 @@ export class BMapComponent implements OnInit {
     map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
 
     //this.adjustMap(); // 调整地图大小
-    this.addPoint(map, this.gyms); // 运行加点函数
+    this.addPoint(map, this.gymsLocation); // 运行加点函数
   }
 
   adjustMap() {
@@ -51,6 +65,24 @@ export class BMapComponent implements OnInit {
     });
   }
 
+  creategymInfoWindow(map): any {
+    let IBOption = {
+        width: 320,
+        height: 200,
+        title: "key",
+        panel  : "panel",         //检索结果面板
+        enableAutoPan : true,     //自动平移
+        searchTypes : [
+            BMAPLIB_TAB_SEARCH,   //周边检索
+            BMAPLIB_TAB_TO_HERE,  //到这里去
+            BMAPLIB_TAB_FROM_HERE //从这里出发
+        ]
+    };
+    let infoContent = "<div>Du niang 250</div>"
+    let gymInfoWindow = new BMapLib.SearchInfoWindow(map, infoContent, IBOption);
+    return gymInfoWindow;
+  }
+
   /* 加点大函数 */
   addPoint(map: any, data: any) {
     // 设置图标
@@ -59,13 +91,15 @@ export class BMapComponent implements OnInit {
 			offset: new BMap.Size(10, 25),    
 				// 设置图片偏移。   				// 当您需要从一幅较大的图片中截取某部分作为标注图标时，您   				// 需要指定大图的偏移位置，此做法与css sprites技术类似。    				// imageOffset: new BMap.Size(0, 0 - index * 25)   // 设置图片偏移    
 		});
+
+    let that = this;
     
     // 加点
     
     // 访问字典的模板 请大神无视
     // for (let key in data) {
     //   let value = data[key]
-    // }
+    // }var searchInfoWindow = null;
 
     for (let id in data) {
       let vLati = data[id]["lati"];
@@ -76,6 +110,28 @@ export class BMapComponent implements OnInit {
       // 设置每一个标记点位
       let bMarker = new BMap.Marker(bPoint, {icon: bIcon});
       bMarker.label = id;
+
+      // 设置事件。点击某个点弹出信息框
+      bMarker.addEventListener("click", function() {
+        // let gymInfoWindow = that.creategymInfoWindow(map);
+        // gymInfoWindow.open(bMarker);
+        let IBOption = {
+            width: 320,
+            height: 200,
+            title: "key",
+            panel  : "panel",         //检索结果面板
+            enableAutoPan : true,     //自动平移
+            searchTypes : [
+                BMAPLIB_TAB_SEARCH,   //周边检索
+                BMAPLIB_TAB_TO_HERE,  //到这里去
+                BMAPLIB_TAB_FROM_HERE //从这里出发
+            ]
+        };
+        let infoContent = "<div>Du niang 250</div>"
+        let gymInfoWindow = new BMapLib.SearchInfoWindow(map, infoContent, IBOption);
+        gymInfoWindow.open(bMarker);
+
+      });
 
       // 把每个点摆到地图上
       map.addOverlay(bMarker);
