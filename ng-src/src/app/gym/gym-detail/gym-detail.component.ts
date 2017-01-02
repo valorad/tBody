bf273ee35f0872d0e255b2b732fe9fc1968df426
Valorad <wcxaaa@gmail.com>
2017-01-02 21:44:10 +0800

    * (WIP)尝试detail页面通数据

--- ng-src/src/app/gym/gym-detail/gym-detail.component.ts
+++ ng-src/src/app/gym/gym-detail/gym-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+
+interface IGymLocation {
+  lati: number;
+  longi: number;
+}
 
 @Component({
   selector: 'app-gym-detail',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GymDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, router: Router) { }
 
   ngOnInit() {
+     let id = this.getId();
+     //subscribe一下gymserice，然后选择一下得到数据
+     let location: IGymLocation;
+    //  location.lati = 30.111;
+    //  location.longi = 150.8888;
+    //  let description = "observable的数据";
+
+  }
+
+  getId(): string {
+    let gymID: string;
+    this.route.params.subscribe(
+      (params: Params) => {
+        let id = params['id']
+        gymID = id;
+      }
+    );
+    return gymID;
   }
 
 }
