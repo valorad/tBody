c69e8cd905f368835098bde1f85c1ff267217ebe
Valorad <wcxaaa@gmail.com>
2017-01-03 11:48:25 +0800

    * [WIP]详情页面的数据传入

--- ng-src/src/app/gym/gym-detail/gym-detail.component.ts
+++ ng-src/src/app/gym/gym-detail/gym-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 
+import { GymService } from '../gym.service';
+
 interface IGymLocation {
   lati: number;
   longi: number;
@@ -9,17 +11,24 @@ interface IGymLocation {
 @Component({
   selector: 'app-gym-detail',
   templateUrl: './gym-detail.component.html',
-  styleUrls: ['./gym-detail.component.scss']
+  styleUrls: ['./gym-detail.component.scss'],
+  providers:[GymService]
 })
 export class GymDetailComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute, router: Router) { }
+  public id: string;
+  public gyms = [];
+  public error: string;
+  constructor(private route: ActivatedRoute, router: Router, private gymService: GymService) { }
 
   ngOnInit() {
-     let id = this.getId();
+     this.id = this.getId();
      //subscribe一下gymserice，然后选择一下得到数据
+     this.gymService.getGymList().subscribe(
+       (resGyms) => this.gyms = resGyms,
+       (resError) => this.error = resError
+     );
      let location: IGymLocation;
-    //  location.lati = 30.111;
+     //location.lati = this.gyms[id];
     //  location.longi = 150.8888;
     //  let description = "observable的数据";
 
